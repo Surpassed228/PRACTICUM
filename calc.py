a2e9b1dcdaea3528d6c86c0418024d1e06a82eea
Luchian <luchian-1999@mail.ru>
2024-02-16 12:14:24 +0300

    Some changes to the files

--- calc.py
+++ calc.py
@@ -1,3 +1,4 @@
 a = float(input('Enter first number: '))
 b = float(input('Enter second number: '))
 print(f"The result of summation is {a+b}")
+print('Actually,I have changed someting just in order to check')
